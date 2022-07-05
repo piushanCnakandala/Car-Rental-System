649fbfcc37f03699732f6b97ab3ebffbdea27dba
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-07-05 23:22:18 +0530

    Aded JPA Config and imported it to the WebRootConfg

---
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
@@ -0,0 +1,7 @@
+package lk.Spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JPAConfig {
+}
---
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/config/WebRootConfig.java
@@ -0,0 +1,11 @@
+package lk.Spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAConfig.class)
+public class WebRootConfig {
+
+}
