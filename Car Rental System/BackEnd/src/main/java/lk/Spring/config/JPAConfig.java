69a787a00f373018318f670a00c800f8942629ad
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-07-07 00:09:16 +0530

    import application.properties

--- Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
@@ -1,8 +1,10 @@
 package lk.Spring.config;
 
-import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jca.support.LocalConnectionFactoryBean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -14,43 +16,47 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "lk.Spring.repo") //query liyana palce eka (repo) interdusing
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
+    @Autowired
+    Environment environment;
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
         bean.setJpaVendorAdapter(va);
         bean.setDataSource(ds);
-        bean.setPackagesToScan("lk.Spring.entity"); //location in entity
+        bean.setPackagesToScan(environment.getRequiredProperty("entity.package.name")); //location in entity
         return bean;
     }
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/easyCarRental?createDatabasesIfNotExist=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("12345");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(environment.getRequiredProperty("my.app.url"));
+        dataSource.setUsername(environment.getRequiredProperty("my.app.username"));
+        dataSource.setPassword(environment.getRequiredProperty("my.app.password"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("my.app.driverclassname"));
         return dataSource;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter(){
         HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
-        vendor.setDatabasePlatform("org.hibernate.MySQL8Dialect");
+        vendor.setDatabasePlatform(environment.getRequiredProperty("my.app.dialect"));
         vendor.setDatabase(Database.MYSQL);
         vendor.setShowSql(true);
         vendor.setGenerateDdl(true);
         return vendor;
 
     }
-/*@Bean
+@Bean
   public  PlatformTransactionManager transactionManager(EntityManagerFactory emf){
         return  new JpaTransactionManager(emf);
-  }*/
+  }
 }
