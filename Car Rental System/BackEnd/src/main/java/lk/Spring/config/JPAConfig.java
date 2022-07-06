64a4abcc25d72d6053be222d85b7004b8f81641e
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-07-06 12:16:35 +0530

    introduse the transaction manager

--- Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
@@ -1,13 +1,16 @@
 package lk.Spring.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jca.support.LocalConnectionFactoryBean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -42,4 +45,8 @@ public class JPAConfig {
         return vendor;
 
     }
+@Bean
+  public  PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return  new JpaTransactionManager(emf);
+  }
 }
