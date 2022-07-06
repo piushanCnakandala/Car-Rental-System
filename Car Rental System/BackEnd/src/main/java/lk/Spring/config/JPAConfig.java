5b18a23d5e90d3843a9b22521a3b81f04be80ac4
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-07-06 12:01:07 +0530

    introduce Jpa vendor

--- Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
@@ -1,7 +1,45 @@
 package lk.Spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jca.support.LocalConnectionFactoryBean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(va);
+        bean.setDataSource(ds);
+        bean.setPackagesToScan("lk.Spring.entity");
+        return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/easyCarRental?createDatabasesIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
+        vendor.setDatabasePlatform("org.hibernate.MySQL8Dialect");
+        vendor.setDatabase(Database.MYSQL);
+        vendor.setShowSql(true);
+        vendor.setGenerateDdl(true);
+        return vendor;
+
+    }
 }
