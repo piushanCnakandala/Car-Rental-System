207852e1d7d9fadd7224d96524201e7d5fb16f43
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-07-07 23:57:48 +0530

    customer repo created

--- Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/config/JPAConfig.java
@@ -20,7 +20,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
-//@EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
+@EnableJpaRepositories(basePackages = "lk.Spring.repo")
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 public class JPAConfig {
---
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/repo/CustomerRepo.java
@@ -0,0 +1,7 @@
+package lk.Spring.repo;
+
+import lk.Spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer,String> {
+}
