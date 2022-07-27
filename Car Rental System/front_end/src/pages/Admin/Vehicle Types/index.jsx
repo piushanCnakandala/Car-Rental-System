a426eaa222175fc36492cf6c17df26c8c701e2f5
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-07-28 02:41:02 +0530

    update vehicle types

--- Car Rental System/front_end/src/pages/Admin/Vehicle Types/index.jsx
+++ Car Rental System/front_end/src/pages/Admin/Vehicle Types/index.jsx
@@ -1,4 +1,4 @@
-import {Dialog, DialogContent, DialogTitle, Grid, IconButton, Typography,} from "@mui/material";
+import {Dialog, DialogContent, DialogTitle, Grid, IconButton, Tooltip, Typography,} from "@mui/material";
 import React, {Component} from "react";
 import Navbar from "../../../components/common/Navbar/Admin";
 import Sidebar from "../../../components/common/Sidebar";
@@ -9,6 +9,7 @@ import {withStyles} from "@mui/styles";
 import {styleSheet} from "./styles";
 import CloseIcon from "@mui/icons-material/Close";
 import AddVehicleType from "../../../components/AddVehicleType";
+import VehicleTypeService from "../../../services/VehicleTypeService";
 
 class VehicleType extends Component {
     constructor(props) {
@@ -26,19 +27,19 @@ class VehicleType extends Component {
             //  for data table
             columns: [
                 {
-                    field: "registrationNumber",
+                    field: "vehicle_Type_Id",
                     headerName: "Vehicle Type ID",
                     width: 400,
                 },
 
                 {
-                    field: "Type",
+                    field: "type",
                     headerName: "Type",
                     width: 400,
                 },
 
                 {
-                    field: "L.D.W.",
+                    field: "loss_Damage_Waiver",
                     headerName: "L.D.W.",
                     width: 400,
                     sortable: false,
@@ -48,24 +49,91 @@ class VehicleType extends Component {
                     field: "Action",
                     headerName: "Action",
                     width: 400,
+                    renderCell: (params) => {
+                        return (
+                            <>
+                                <Tooltip title="Edit">
+                                    <IconButton onClick={async () => {
+                                        await this.updateVehicleType(params.row);
+                                    }}>
+                                        <EditIcon className={'text-blue-500'}/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Delete">
+                                    <IconButton onClick={async () => {
+                                        await this.deleteVehicleType(params.row.vehicle_Type_Id);
+                                    }}>
+                                        <DeleteIcon className={'text-red-500'}/>
+                                    </IconButton>
+                                </Tooltip>
+                            </>
+                        )
+                    }
                 },
             ],
         };
     }
 
-    async loadData() {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.popup == true){
+            this.loadVtypeData()
+        }
+    }
+
+    deleteVehicleType = async (id) => {
+        let params = {
+            typeId: id
+        }
+        let res = await VehicleTypeService.deleteVehicleType(params)
+        if (res.status === 200){
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'success'
+            })
+            this.loadVtypeData()
+        }else {
+            this.setState({
+                alert: true,
+                message: res.data.message,
+                severity: 'error'
+            })
+        }
+    }
+
+    updateVehicleType = async (data) => {
+        const row = data;
+        let updateVehicleType = {
+            "vehicle_Type_Id": row.vehicle_Type_Id,
+            "type": row.type,
+            "loss_Damage_Waiver": row.loss_Damage_Waiver,
+        }
+        await this.setState({updateVehicleType: updateVehicleType})
+        await this.setState({
+            popup: true,
+            isUpdate: true
+        })
+    }
+
+    async loadVtypeData() {
         // let resp = await PostService.fetchPosts();
-        const data = [];
-        this.setState({
-            loaded: true,
-            data: data,
-        });
-        console.log(this.state.data);
-        // console.log(JSON.stringify(resp.data));
+        let resp = await VehicleTypeService.fetchVehicleType();
+        let nData = [];
+        if (resp.status === 200) {
+            resp.data.data.map((value, index) => {
+                value.id = value.vehicle_Type_Id;
+                nData.push(value)
+            })
+
+            this.setState({
+                loaded: true,
+                data: nData,
+            });
+        }
     }
 
     componentDidMount() {
-        this.loadData();
+        this.loadVtypeData();
         console.log("Mounted");
     }
 
