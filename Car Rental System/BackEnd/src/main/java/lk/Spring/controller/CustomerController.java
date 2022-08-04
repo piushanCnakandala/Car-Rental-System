f0ae74e1eede78aa3841d85cd3f36f310c8381af
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-04 13:14:23 +0530

    update

--- Car Rental System/BackEnd/src/main/java/lk/Spring/controller/CustomerController.java
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/customer")
@@ -43,4 +44,21 @@ public class CustomerController {
     public ResponseUtil getAllCustomer(){
         return  new ResponseUtil(200,"Ok",customerService.getAllCustomer());
     }
+
+    @GetMapping(path = "/count", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil userCount() {
+        return new ResponseUtil(200, "Ok", customerService.countUsers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil uploadFile(@RequestParam("file") MultipartFile myFile, @RequestParam("customer") String customer){
+        System.out.println("Hey");
+        System.out.println(customer);
+        System.out.println(myFile.getName());
+
+        customerService.saveCustomerWithImg(customer,myFile);
+        return new ResponseUtil(200, "New User Create Successfully", null);
+    }
+
 }
