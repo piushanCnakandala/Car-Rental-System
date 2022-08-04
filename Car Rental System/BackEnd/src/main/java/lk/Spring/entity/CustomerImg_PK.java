5c967be75115b544a62cc652a4ab2a3777a760b9
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-04 12:47:47 +0530

    CustomerImg_PK create

---
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/entity/CustomerImg_PK.java
@@ -0,0 +1,13 @@
+package lk.Spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerImg_PK {
+    private String imgId;
+    private String customerID;
+}
