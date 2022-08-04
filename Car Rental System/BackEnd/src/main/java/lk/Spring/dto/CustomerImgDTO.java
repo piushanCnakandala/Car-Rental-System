839f9621fe8a79221bd5e075d1e93d367e013e84
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-04 13:01:54 +0530

    CustomerImgDTO create

---
+++ Car Rental System/BackEnd/src/main/java/lk/Spring/dto/CustomerImgDTO.java
@@ -0,0 +1,16 @@
+package lk.Spring.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class CustomerImgDTO {
+    private String imgId;
+    private String path;
+}
